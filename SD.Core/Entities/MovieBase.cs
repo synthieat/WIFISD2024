52a11df0c8a31194094dce5f7222480be456296a
Horst Werner Schneider <horst.schneider@vlv.at>
2024-04-11 13:54:31 +0200

    Add project files.

---
+++ SD.Core/Entities/MovieBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD.Core.Entities
+{
+    public class MovieBase
+    {
+        [Key] /* Wäre hier jetzt überflüssig */
+        public Guid Id { get; set; }
+        [MaxLength(128)]
+        [Required]
+        [MinLength(2)]
+        public string Title { get; set; }
+
+        public int GenreId { get; set; }    
+
+        public string MediumTypeCode { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
+        public decimal Price { get; set; }
+
+        [Column(TypeName = "date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
