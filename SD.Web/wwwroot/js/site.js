554bf75b062b96a76fa3edf64527c40029a3fb13
Horst Werner Schneider <horst.schneider@vlv.at>
2024-05-27 09:33:48 +0200

    minor changes

--- SD.Web/wwwroot/js/site.js
+++ SD.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    var movieTable = document.getElementById('movieTable');
+//    var deleteAnchors1;
+//    if (movieTable) {
+//        deleteAnchors1 = movieTable.querySelectorAll('a.fa-remove');
+//    }
+
+//    deleteAnchors1.forEach(function(e){
+//        e.addEventListener("click", function () {
+//            alert("hallo world");
+//            event.preventDefault();
+
+//        });        
+//    })  
+//});
